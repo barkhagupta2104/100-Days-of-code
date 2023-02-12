225971b12cad1ea4470a67fefe0d9fd87876ee23
BARKHA GUPTA <114460970+barkhagupta2104@users.noreply.github.com>
2023-02-12 16:28:23 +0530

    Create peak index in a mountain array.cpp

---
+++ day22/peak index in a mountain array.cpp
@@ -0,0 +1,11 @@
+    int peakIndexInMountainArray(vector<int>& arr)
+     { int i;
+        for( i=0;i<arr.size();i++)
+        {
+            if(arr[i]<arr[i+1])
+
+            {}
+            else 
+            return i;
+        }
+        return i;
