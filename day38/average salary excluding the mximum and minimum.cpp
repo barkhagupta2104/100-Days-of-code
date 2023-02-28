88cd2e3b923e5f47979437f638e24916d87fc53a
BARKHA GUPTA <114460970+barkhagupta2104@users.noreply.github.com>
2023-03-01 01:49:53 +0530

    Create average salary excluding the mximum and minimum.cpp

---
+++ day38/average salary excluding the mximum and minimum.cpp
@@ -0,0 +1,11 @@
+ double average(vector<int>& salary)
+     {
+        sort(salary.begin(),salary.end());
+        double sum=0;
+        for(int i=1;i<salary.size()-1;i++)
+        {
+            sum=sum+salary[i];
+        }
+        return (sum/(salary.size()-2));
+    }
+};
