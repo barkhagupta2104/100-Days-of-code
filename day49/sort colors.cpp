40ab6cdc9dea02ea7d47eb3b410cbbfec2ccb902
BARKHA GUPTA <114460970+barkhagupta2104@users.noreply.github.com>
2023-03-12 22:58:42 +0530

    Create sort colors.cpp

---
+++ day49/sort colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+       int temp=0;
+       for(int i=0;i<nums.size();i++)
+       {
+           for(int j=i+1;j<nums.size();j++)
+           {
+               if(nums[i]>nums[j])
+               {
+                   temp=nums[i];
+                   nums[i]=nums[j];
+                   nums[j]=temp;
+               }
+
+           }
+       } 
+     
+    }
+};
+Console
