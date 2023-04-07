57f0296a35c3310974463f7515ac57af1ccb5a7d
BARKHA GUPTA <114460970+barkhagupta2104@users.noreply.github.com>
2023-04-07 21:23:58 +0530

    Create search in 2d matrix.cpp

---
+++ day74/search in 2d matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int m=matrix.size();
+         int n=matrix[0].size();
+        int start=0;
+        int end=m*n-1;
+        while(start<=end)
+        {
+
+            int mid=start+(end-start)/2;
+            if(matrix[mid/n][mid%n]==target)
+             return true;
+            else if(matrix[mid/n][mid%n]<target)
+            start=mid+1;
+            else
+            end=mid-1;
+        }
+        return false;
+    }
+};
